a6a3b326e0c44d5955aaa44f772f7a77c83823bf
Mariia2809 <popovamariia2809@gmail.com>
2024-03-19 18:52:34 +0700

    end

--- cypress/e2e/spec-copy-1.cy.js
+++ cypress/e2e/spec-copy-1.cy.js
@@ -6,6 +6,20 @@ describe('template spec', () => {
     cy.get('.form-input--text').type('testerStudent')
     cy.get('.form-input--password').type('Password1')
     cy.get(':nth-child(3) > .button').click()
+    // cy.get('[data-v-4cad5e75=""][data-v-97a96b5c=""]').should('exist')
 
+    // cy.get(':nth-child(1) > .form-input--password').type('QWEasd123');
+    // cy.wait(2000)
+    // cy.get(':nth-child(2) > .form-input--password').type('QWEasd1234');
+    // cy.get('.form__buttons > div > .button').click();
+    // cy.wait(2000)
+    // cy.get('.form-error > span').should('exist')
+
+    cy.get(':nth-child(1) > .form-input--password').type('QWEasd1234');
+    cy.wait(2000)
+    cy.get(':nth-child(2) > .form-input--password').type('QWEasd1234');
+    cy.get('.form__buttons > div > .button').click();
+    cy.wait(2000)
+    cy.get('.form__buttons > div > p').should('exist')
   })
 })
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -10,14 +10,5 @@ describe('template spec', () => {
     cy.get(':nth-child(3) > .button').click()
 
     cy.get('.form-error').should('exist')
-  // it('passes', () => {
-  //   cy.visit('https://dev.profteam.su/')
-  //   cy.get('[href="/password"] > .button').click()
-  //
-  //   cy.get('.form-input--text').type('testerStudent')
-  //
-  //   cy.get('.form-input--password').type('Password1')
-  //   cy.get(':nth-child(3) > .button').click()
-
   })
 })
