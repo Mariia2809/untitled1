5af2b63849d5efa20b637c05a06ff2873082ce49
Mariia2809 <popovamariia2809@gmail.com>
2024-03-20 16:24:23 +0700

    day2

--- cypress/e2e/spec-copy-1.cy.js
+++ cypress/e2e/spec-copy-1.cy.js
@@ -1,23 +1,35 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://dev.profteam.su/')
-    cy.get('[href="/login"] > .button').click()
 
-    cy.get('.form-input--text').type('testerStudent')
-    cy.get('.form-input--password').type('Password1')
-    cy.get(':nth-child(3) > .button').click()
-    // cy.get('[data-v-4cad5e75=""][data-v-97a96b5c=""]').should('exist')
+//базовая успешная авторизация
 
-    // cy.get(':nth-child(1) > .form-input--password').type('QWEasd123');
-    // cy.wait(2000)
-    // cy.get(':nth-child(2) > .form-input--password').type('QWEasd1234');
-    // cy.get('.form__buttons > div > .button').click();
-    // cy.wait(2000)
-    // cy.get('.form-error > span').should('exist')
+    cy.visit('https://dev.profteam.su');
+// cy.wait(2000);
+    cy.get('[href="/login"] > .button').click();
+// cy.wait(2000);
+    cy.get('.form-input--text').type('testerInstitution');
+// cy.wait(2000);
+    cy.get('.form-input--password').type('Password1');
+// cy.wait(2000);
+    cy.get(':nth-child(3) > .button').click();
+    cy.wait(2000);
+
+// успешное изменение пароля
 
     cy.get(':nth-child(1) > .form-input--password').type('QWEasd1234');
     cy.wait(2000)
     cy.get(':nth-child(2) > .form-input--password').type('QWEasd1234');
+    cy.get('.form__buttons > div > .button').click();
+    cy.wait(2000)
+
+
+// возвращение старого пароля чтобы не было ошибок при входе по данным из ТЗ
+
+    cy.get(':nth-child(1) > .header__nav > [href="/vacancies"] > .header__label').click()
+    cy.go('back')
+    cy.get(':nth-child(1) > .form-input--password').type('Password1');
+    cy.wait(2000)
+    cy.get(':nth-child(2) > .form-input--password').type('Password1');
     cy.get('.form__buttons > div > .button').click();
     cy.wait(2000)
     cy.get('.form__buttons > div > p').should('exist')
--- cypress/e2e/spec-copy-6.cy.js
+++ cypress/e2e/spec-copy-6.cy.js
@@ -1,17 +1,22 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://dev.profteam.su/')
-// Добавление роли студенту
-    cy.get('[href="/login"] > .button').click()
-    cy.wait(1000)
-    cy.get('.form-input--text').type('LoraStratos')
-    cy.get('.form-input--password').type('Eujkm1') // Уголь1
-    cy.wait(1000)
-    cy.get(':nth-child(3) > .button').click()
-    cy.wait(1000)
+
+    //базовая успешная авторизация
+
+    cy.visit('https://dev.profteam.su');
+    // cy.wait(2000);
+    cy.get('[href="/login"] > .button').click();
+    // cy.wait(2000);
+    cy.get('.form-input--text').type('testerAdmin');
+    // cy.wait(2000);
+    cy.get('.form-input--password').type('Password1');
+    // cy.wait(2000);
+    cy.get(':nth-child(3) > .button').click();
+    cy.wait(2000);
+
     cy.get('.header-container__desktop > .notification-bell > .notification-bell__icon__desktop > svg').click()
     cy.get('.header-container__desktop > .notification-bell > .notification-bell__similar > .notifications-similar > .notifications-similar__header > .link').click()
-    cy.log('Уведомления открываются и прочитываются!')
-    cy.get('.header-container__desktop > .notification-bell > .notification-bell__similar > .notifications-similar > .notifications-similar__header > .notification-count').should('exist')
+    cy.get('.header-container__desktop > .notification-bell > .notification-bell__similar > .notifications-similar > .search-title').should('exist')
+
   })
 })
